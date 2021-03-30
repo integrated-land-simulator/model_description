61f7d318fdcb7db0698978d1207bafe13549600e
Akira Takeshima <akira@rainbow.iis.u-tokyo.ac.jp>
2021-03-30 12:29:15 +0900

    removed

--- convert/md2html.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-filelist=$(ls *.md | rev | cut -c 4-| rev)
-echo ${filelist}
-for name in ${filelist}
-do
-pandoc -f markdown ${name}.md  -t html --template=template.html -o html_en/${name}.html --metadata title="MIROC-DOC ${name}" -c github-markdown.css
-done
-
-rm tmp*
